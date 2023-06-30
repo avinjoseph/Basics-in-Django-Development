700f71fc514e33c154c0d51e18b324809c8a0472
Avin Joseph <avin.chelseafc@gmail.com>
2023-06-30 23:39:30 +0530

    Day 3 - Added urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from boards import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home, name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^admin/',admin.site.urls),
 ]
