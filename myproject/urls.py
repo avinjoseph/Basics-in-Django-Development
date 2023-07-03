fcea233eb82d100399045008a73610425426b6b2
Avin Joseph <avin.chelseafc@gmail.com>
2023-07-03 20:56:15 +0530

    Different types of url methods

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
+from django.urls import re_path
 from boards import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
-    url(r'^admin/',admin.site.urls),
+    re_path(r'^$', views.home, name='home'),
+    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^admin/',admin.site.urls),
 ]
