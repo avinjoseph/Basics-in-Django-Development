e2a1bc839b5e93c4b93bb0baa7a3b695afdcce28
Avin Joseph <avin.chelseafc@gmail.com>
2023-07-18 08:44:46 +0530

    Day 4 - Added the signup page

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import re_path
 from boards import views
+from accounts import views as account_views
 
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
+    re_path(r'^signup/$',account_views.signup, name='signup'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(r'^admin/',admin.site.urls),
