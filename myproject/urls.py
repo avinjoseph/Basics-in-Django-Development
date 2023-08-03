b0e22c0d06229ca5a4a4881b0b7cdcbe8d070880
Avin Joseph <avin.chelseafc@gmail.com>
2023-08-03 22:44:41 +0530

    Day 4 - Added static files and done logoutand singup

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import re_path
 from boards import views
+from django.contrib.auth import views as auth_views
 from accounts import views as account_views
 
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
     re_path(r'^signup/$',account_views.signup, name='signup'),
+    re_path(r'^logout/$',auth_views.LogoutView.as_view(), name='logout'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(r'^admin/',admin.site.urls),
